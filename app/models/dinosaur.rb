0b4e58b9c3b696736d328512c65da0e26e2b8a20
Abohte <adinda.bohte@gmail.com>
2017-12-08 15:40:39 +0100

    Add dinosaur validations

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,4 +1,9 @@
 class Dinosaur < ApplicationRecord
+  validates :name, :age, presence: true
+  validates :name, uniqueness: true
+
+  validates :age, length: { maximum: 5 }
+  validates :image_url, length: { maximum: 255 }, allow_blank: true
 
   scope :order_by_name, -> { order(:name) }
 
